92c12a2fdd3662a512270fd26557afc41fe36a7f
Ethan Le <ethanle@unc.edu>
2026-08-20 23:44:32 -0400

    implement callback route after authorization

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,32 @@
 import express from 'express'
 import pool from './db.js'
+import axios from 'axios'
 
 const app = express()
 
 app.use(express.json())
 
-app.get("/db-health", async (req, res) => {
-    const result = await pool.query('SELECT NOW()')
-    res.json({ time: result.rows[0] })
+app.get("/auth/callback", async (req, res) => {
+    const code = req.query.code
+
+    const params = new URLSearchParams({
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: process.env.SPOTIFY_REDIRECT_URI
+    })
+
+    const response = await axios.post(
+        'https://accounts.spotify.com/api/token',
+        params.toString(),
+        {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic ' + Buffer.from(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64')
+            }
+        }
+    )
+
+    res.json(response.data)
 })
 
 app.get("/auth/login", (req, res) => {
