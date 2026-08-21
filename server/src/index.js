013c3abd5ad30d82a216ed158594dcd4ed565fcb
Ethan Le <ethanle@unc.edu>
2026-08-20 22:54:14 -0400

    added spotify redirection route for authorization

--- server/src/index.js
+++ server/src/index.js
@@ -5,13 +5,20 @@ const app = express()
 
 app.use(express.json())
 
-app.get("/health", (req, res) => {
-    res.json({ status: 'ok'})
-})
-
 app.get("/db-health", async (req, res) => {
     const result = await pool.query('SELECT NOW()')
     res.json({ time: result.rows[0] })
+})
+
+app.get("/auth/login", (req, res) => {
+    const params = new URLSearchParams({
+        client_id: process.env.SPOTIFY_CLIENT_ID,
+        response_type: 'code',
+        redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+        scope: 'user-read-recently-played'
+    })
+
+    res.redirect(`https://accounts.spotify.com/authorize?${params.toString()}`)
 })
 
 const PORT = process.env.PORT || 3001
