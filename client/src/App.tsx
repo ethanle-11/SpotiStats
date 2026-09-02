80c1dfdbf6195b4137f69db8cb6e8f507df4a85d
Ethan Le <ethanle@unc.edu>
2026-09-01 23:26:05 -0400

    added login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
 
 
-function App() {
-  
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
-export default App
